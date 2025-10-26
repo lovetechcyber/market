376479937deca16c5cefc251799cdc0c602de8ae
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-10-26 10:34:42 +0100

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://market-e50k.onrender.com/api",
   withCredentials: true, // important for cookies
 });
 

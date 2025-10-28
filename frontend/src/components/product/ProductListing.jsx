9e1f63c4d44361294ae1216fddcd780fe3c8c899
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-10-28 10:02:13 +0100

    Update ProductListing.jsx

--- frontend/src/components/product/ProductListing.jsx
+++ frontend/src/components/product/ProductListing.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import api from '../../api/axios';
+import {api} from '../../api/axios';
 
 export default function ProductListing() {
   const [products, setProducts] = useState([]);

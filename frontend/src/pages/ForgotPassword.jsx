4fdbc846d5af866acc64266448a9e5d031bfe4b9
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-10-28 09:57:19 +0100

    Update ForgotPassword.jsx

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../api/axios";
+import {api} from "../api/axios";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");

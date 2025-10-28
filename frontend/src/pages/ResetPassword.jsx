318154a4cb6610a7c60cc96bc64b863471ad5604
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-10-28 09:59:00 +0100

    Update ResetPassword.jsx

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import api from "../api/axios";
+import {api} from "../api/axios";
 import { useParams, useNavigate } from "react-router-dom";
 import PasswordStrength from "../components/PasswordStrength";
 
